7ec440281d6033b828ea54c6127a67134c0ec328
fabiano de Souza <fabianocs78@outlook.com>
2019-07-10 22:48:31 -0400

    criado a autenticacao backend

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,16 +1,29 @@
 const express = require('express')
+const auth = require('./auth')
 
-module.exports = function(server) {
- //API routes
-  const router = express.Router()
-  server.use('/api', router)
+module.exports = function (server) {
+    /*  
+    * Rotas abertas  
+    */  
+  const openApi = express.Router()  
+  server.use('/oapi', openApi)
 
-  // first route
-  const billinCycleService = require('../api/billingCycle/billingCycleService')
-  billinCycleService.register(router, '/billingCycles')
+  const AuthService = require('../api/user/authService')  
+  openApi.post('/login', AuthService.login)  
+  openApi.post('/signup', AuthService.signup)  
+  openApi.post('/validateToken', AuthService.validateToken)
 
+   /*  
+   * Rotas protegidas por Token JWT  
+   */  
+  const protectedApi = express.Router()  
+  server.use('/api', protectedApi)
 
-  const billingSummaryService = require('../api/billingSummary/billingSummaryService')
-  router.route('/billingSummary').get(billingSummaryService.getSummary)
-  //billingSummaryService.register(router, '/billingSummary')
+  protectedApi.use(auth)
+
+  const billingCycleService = require( '../api/billingCycle/billingCycleService')  
+  billingCycleService.register(protectedApi, '/billingCycles')
+
+  const billingSummaryService = require('../api/billingSummary/billingSummaryService')  
+  protectedApi.route('/billingSummary').get(billingSummaryService.getSummary)
 }
